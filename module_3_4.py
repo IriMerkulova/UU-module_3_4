ae1eb68d877a292151634cd870895e2648b79079
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-21 21:06:11 +0300

    Update module_3_4.py
    исправление недочетов, замена print на return(возврат).

--- module_3_4.py
+++ module_3_4.py
@@ -2,13 +2,12 @@ def single_root_words(root_word, *other_words):
     same_words = []
 
     for i in range(len(other_words)):
-        if root_word in other_words[i]:
-            same_words.append(other_words[i])
         up_other_words = str(other_words[i])
+        if root_word.upper() in up_other_words.upper():
+            same_words.append(other_words[i])
         if up_other_words.upper() in root_word.upper():
             same_words.append(other_words[i])
-    i += 1
-    print(same_words)
+    return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
